d9dd13ea69da01d1640914afe67ca338f968f1e0
Hanander <EShunin@croc.ru>
2020-05-17 21:18:51 +0300

    added CardManager

--- src/Cards.py
+++ src/Cards.py
@@ -20,3 +20,44 @@ class Card:
 
             'img link': None
         }
+
+class CardManager:
+    # ------------------------------------------------------------------------------------------------------------------
+    # Public methods
+    @staticmethod
+    def FilterCards(cardsJSON, fields):
+        # iterate over cards and apply filter by each field
+        filtredKeys = []
+        for cardKey in cardsJSON.keys():
+            for fieldName in fields.keys():
+                if CardManager._FilterByField(cardsJSON[cardKey], fieldName, fields[fieldName]):
+                    filtredKeys.append(cardKey)
+        
+        return {cardKey: cardsJSON[cardKey] for cardKey in filtredKeys}
+                
+
+    @staticmethod
+    def GetImages(cardsJSON, imgFolder):
+        pass
+    
+    # ------------------------------------------------------------------------------------------------------------------
+    # Private methods
+    
+    @staticmethod
+    def _FilterByField(card, fieldName, fieldVals):
+        # check field is not empty
+        if fieldName == 'abilities':
+            if len(card[fieldName]) == 0:
+                return False
+        else:
+            if card[fieldName] is None:
+                return False
+        # check card has field val
+        for val in fieldVals:
+            if fieldName == 'abilities':
+                if val in card[fieldName]:
+                    return True
+            else:
+                if card[fieldName] == val:
+                    return True
+        return False
