1648f2a262bc316c66bdc82bfe4bc410ef97d482
Hanander <EShunin@croc.ru>
2020-05-18 12:57:03 +0300

    added cards loading

--- src/Cards.py
+++ src/Cards.py
@@ -1,9 +1,10 @@
-class Card:
-    def __init__(self):
-        self.id = None
+import os
+import json
 
+class Card:
+    def __init__(self, id=None, params=None):
+        self.id = id
         self.img = None
-    
         self.info = {
             'title': None,
 
@@ -20,10 +21,31 @@ class Card:
 
             'img link': None
         }
+        if not params is None:
+            self._SetInfo(id, params)
+    
+    # ------------------------------------------------------------------------------------------------------------------
+    # Private methods
+    def _SetInfo(self, id, params):
+        for fieldName in params:
+            self.info[fieldName] = params[fieldName]
 
 class CardManager:
     # ------------------------------------------------------------------------------------------------------------------
     # Public methods
+    @staticmethod
+    def LoadCards(cardsFolderPath):
+        cardsInfoFilePath = os.path.join(cardsFolderPath, 'cards_info.json')
+        with open(cardsInfoFilePath, 'r') as f:
+            cardsJSON = json.load(f)
+        cards = []
+        for cardKey in cardsJSON.keys():
+            newCard = Card(id=cardKey, params=cardsJSON[cardKey])
+            cards.append(newCard)
+        
+        return cards
+
+    
     @staticmethod
     def FilterCards(cards, fields):
         # iterate over cards and apply filter by each field
@@ -34,7 +56,6 @@ class CardManager:
                     filtredCards.append(card)
         
         return filtredCards
-                
 
     @staticmethod
     def GetImages(cardsJSON, imgFolder):
@@ -61,3 +82,12 @@ class CardManager:
                 if card.info[fieldName] == val:
                     return True
         return False
+
+if __name__ == "__main__":
+    cardsFolderPath = os.path.join('..', 'cards')
+    cards = CardManager.LoadCards(cardsFolderPath)
+    filtredCards = CardManager.FilterCards(cards, {'type': ['Spell']})
+    print('n filtred cards = {}'.format(len(filtredCards)))
+    print('first 3 card:')
+    for card in filtredCards:
+        print(card.info)
